03e0951d9f3e962ed7dbde5e773f91abacbe4b30
vaheqelyan <martinkenner@mail.com>
2018-12-28 16:26:18 +0400

    Save changes

--- src/xhr/get.ts
+++ src/xhr/get.ts
@@ -1,29 +1,98 @@
+import xhrGet from "./open/get";
 
-export default function get(url, params, parse, encode) {
-	return new Promise((resolve, reject) => {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", url, true);
-
-		xhr.onload = function() {
-			if (this.status == 200) {
-				resolve(this.response);
-			} else {
-				var error:any = new Error(this.statusText);
-				error.code = this.status;
-				reject(error);
-			}
-		};
-
-		xhr.onerror = function() {
-			reject(new Error("Network Error"));
-		};
-
-		if ("timeout" in params) {
-			xhr.timeout = params.timeout;
-			xhr.ontimeout = reject;
-		}
-
-		xhr.send();
-	});
+function clearUrl(url, encodeIndex) {
+	if (url[0] === ";") {
+		url = url.slice(1);
+	}
+	if (encodeIndex !== null) {
+		url = url.slice(0, encodeIndex);
+	}
+	return url;
 }
 
+function getEncode(url) {
+	var index = url.lastIndexOf(" as ");
+	if (index !== -1) {
+		return {
+			encode: url.slice(index + 4, url.length),
+			indexOf: index,
+		};
+	}
+
+	return { encode: null, indexOf: null };
+}
+
+function get(chunks, ...interpolations) {
+	const isBounded = this && this !== window; // plz do not talk me off too much
+
+	var str = "";
+
+	var settings = {};
+
+	for (var i = 0; i < chunks.length; i++) {
+		var key = chunks[i];
+		var value = interpolations[i];
+
+		if (value !== undefined) {
+			if (typeof value === "object") {
+				if (i === 0 && chunks[i + 1].indexOf(isBounded ? ";" : ";http") !== -1) {
+					settings = { ...value };
+					continue;
+				}
+
+				if (key[key.length - 1] === "?") {
+					var generate = Object.keys(value)
+						.map(function(prop) {
+							return [prop, value[prop]].map(encodeURIComponent).join("=");
+						})
+						.join("&");
+					str += `${key}${generate}`;
+				} else {
+					var generate = Object.keys(value)
+						.map(function(prop) {
+							return [prop, value[prop]].map(encodeURIComponent).join("/");
+						})
+						.join("/");
+					str += `${key}${generate}`;
+				}
+
+				// end
+				continue;
+			}
+
+			value += "";
+		} else {
+			str += key;
+			continue;
+		}
+
+		value += "";
+
+		if (/[a-z]/.test(key[key.length - 1])) {
+			str += `${key}/${value}`;
+		} else {
+			str += `${key}${value}`;
+		}
+	}
+
+	var encode = getEncode(str);
+
+	var normalizeUrl = clearUrl(str, encode.indexOf);
+
+	if (isBounded) {
+		if (this.origin) {
+			normalizeUrl = `${this.origin}${normalizeUrl}`;
+		}
+		settings = Object.assign(this, settings);
+	}
+	// headers...
+	settings = {
+		...settings,
+		headers: { ...this.headers },
+	};
+
+	const parseUrl = new URL(normalizeUrl);
+	return xhrGet(normalizeUrl, settings, parseUrl, encode);
+}
+
+export default get;
