2fd96ad9edd3b388af512b81819e25e76bd2caf1
vaheqelyan <martinkenner@mail.com>
2019-01-01 19:12:16 +0400

    Create a file

---
+++ src/help.ts
@@ -0,0 +1,24 @@
+function dataBody(contentType, encode, raw) {
+	if (!encode) {
+		if (/^application\/json/.test(contentType)) {
+			return JSON.parse(raw);
+		}
+	} else {
+		let final = null;
+		switch (encode) {
+			case "json":
+				final = JSON.parse(raw);
+				break;
+
+			case "text":
+				final = raw.toString();
+				break;
+
+			case "raw":
+				final = raw;
+				break;
+		}
+		return final;
+	}
+}
+export default dataBody;
